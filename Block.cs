e126e10502fd0952c89e7c01781eb5e0abb1c803
DESKTOP-UH8UB24\danie <danieltrejoborjas@gmail.com>
2024-08-15 21:03:04 -0600

    New Item class

--- Block.cs
+++ Block.cs
@@ -1,88 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Text.Json;
 
-public class Block
+namespace OkkamiMaker
 {
-    public string Identifier { get; set; }
-    public string Category { get; set; }
-    public string Group { get; set; }
-
-    // Diccionario que almacena los componentes del bloque
-    public Dictionary<string, object> Components { get; private set; } = new Dictionary<string, object>();
-
-    public Block(string identifier, string category, string group)
+    public class Block
     {
-        Identifier = identifier;
-        Category = category;
-    }
+        public string Identifier { get; set; }
+        public string Category { get; set; }
+        public string Group { get; set; }
 
-    // Método para serializar el bloque a JSON
-    public string ToJson()
-    {
-        var blockObject = new
+        // Diccionario que almacena los componentes del bloque
+        public Dictionary<string, object> Components { get; private set; } = new Dictionary<string, object>();
+
+        public Block(string identifier)
         {
-            format_version = "1.20.20",
-            minecraft_block = new
+            Identifier = identifier;
+        }
+
+        // Método para serializar el bloque a JSON
+        public string Compile()
+        {
+            var blockObject = new
             {
-                description = new
+                format_version = "1.20.20",
+                minecraft_block = new
                 {
-                    identifier = Identifier,
-                    menu_category = new
+                    description = new
                     {
-                        category = Category,
-                    }
-                },
-                components = Components
-            }
-        };
-
-        return JsonSerializer.Serialize(blockObject, new JsonSerializerOptions { WriteIndented = true });
-    }
-
-    // Método para agregar el componente "map_color" si es necesario
-    public void MapColor(string color)
-    {
-        if (!string.IsNullOrEmpty(color))
-        {
-            Components["minecraft:map_color"] = color;
-        }
-    }
-
-    // Método para agregar "destructible_by_explosion" si es necesario
-    public void DestructibleByExplosion(float explosionResistance)
-    {
-        Components["minecraft:destructible_by_explosion"] = new { explosion_resistance = explosionResistance };
-    }
-
-    // Método para agregar otros componentes si son necesarios
-    public void CollisionBox(bool hasCollision, float[] origin = null, float[] size = null)
-    {
-        if (hasCollision)
-        {
-            Components["minecraft:collision_box"] = new
-            {
-                origin = origin ?? new float[] { -8.0f, 0.0f, -8.0f },
-                size = size ?? new float[] { 16.0f, 16.0f, 16.0f }
+                        identifier = Identifier,
+                        menu_category = new
+                        {
+                            category = Category
+                        }
+                    },
+                    components = Components
+                }
             };
+
+            return JsonSerializer.Serialize(blockObject, new JsonSerializerOptions { WriteIndented = true });
         }
-        else
+
+        // Método para agregar el componente "map_color" si es necesario
+        public void MapColor(string color)
         {
-            Components["minecraft:collision_box"] = false;
+            if (!string.IsNullOrEmpty(color))
+            {
+                Components["minecraft:map_color"] = color;
+            }
+        }
+
+        // Método para agregar "destructible_by_explosion" con resistencia personalizada
+        public void ExplosionResistance(float resistance)
+        {
+            Components["minecraft:destructible_by_explosion"] = new { explosion_resistance = resistance };
+        }
+
+        // Método para agregar "collision_box"
+        public void CollisionBox(bool hasCollision, float[] origin = null, float[] size = null)
+        {
+            if (hasCollision)
+            {
+                Components["minecraft:collision_box"] = new
+                {
+                    origin = origin ?? new float[] { -8.0f, 0.0f, -8.0f },
+                    size = size ?? new float[] { 16.0f, 16.0f, 16.0f }
+                };
+            }
+            else
+            {
+                Components["minecraft:collision_box"] = false;
+            }
+        }
+
+        // Método para agregar geometría personalizada
+        public void Geometry(string geometryID)
+        {
+            Components["minecraft:geometry"] = geometryID;
+        }
+
+        // Método para agregar la caja de selección
+        public void SelectionBox(bool hasSelectionBox)
+        {
+            Components["minecraft:selection_box"] = hasSelectionBox;
+        }
+
+        // Método para agregar fricción
+        public void Friction(float friction)
+        {
+            Components["minecraft:friction"] = friction;
+        }
+
+        // Método para agregar resistencia al minado
+        public void MiningResistance(float secondsToDestroy)
+        {
+            Components["minecraft:destructible_by_mining"] = new { seconds_to_destroy = secondsToDestroy };
+        }
+
+        // Método para agregar emisión de luz
+        public void LightEmission(int lightEmission)
+        {
+            Components["minecraft:light_emission"] = lightEmission;
+        }
+
+        // Método para definir el nombre visible del bloque
+        public void DisplayName(string displayName)
+        {
+            Components["minecraft:display_name"] = displayName;
+        }
+
+        public void CraftingTable(string TableName, List<string> CraftingTags)
+        {
+            Components["minecraft:crafting_table"] = new { table_name = TableName, crafting_tags = CraftingTags };
         }
     }
-
-    public void Geometry(string geometryID)
-    {
-        Components["minecraft:geometry"] = geometryID;
-    }
-
-    // Método para agregar la caja de selección
-    public void SelectionBox(bool hasSelectionBox)
-    {
-        Components["minecraft:selection_box"] = hasSelectionBox;
-    }
-
-    
 }
---
+++ Item.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace OkkamiMaker
+{
+    public class Item
+    {
+        public string Identifier { get; set; }
+        public string Category { get; set; }
+        public string Group { get; set; }
+
+        // Diccionario que almacena los componentes del ítem
+        public Dictionary<string, object> Components { get; private set; } = new Dictionary<string, object>();
+
+        public Item(string identifier, string category, string group)
+        {
+            Identifier = identifier;
+            Category = category;
+            Group = group;
+        }
+
+        public string Compile()
+        {
+            var itemObject = new
+            {
+                format_version = "1.20.50",
+                minecraft_item = new
+                {
+                    description = new
+                    {
+                        identifier = Identifier,
+                        menu_category = new
+                        {
+                            category = Category,
+                        }
+                    },
+                    components = Components
+                }
+            };
+
+            return JsonSerializer.Serialize(itemObject, new JsonSerializerOptions { WriteIndented = true });
+        }
+
+        // Métodos para agregar componentes
+        public void Icon(string texture)
+        {
+            Components["minecraft:icon"] = new { texture };
+        }
+
+        public void AllowOffHand(bool value)
+        {
+            Components["minecraft:allow_off_hand"] = new { value };
+        }
+
+        public void BlockPlacer(string block, params string[] useOn)
+        {
+            Components["minecraft:block_placer"] = new
+            {
+                block,
+                use_on = useOn
+            };
+        }
+
+        public void CanDestroyInCreative(bool value)
+        {
+            Components["minecraft:can_destroy_in_creative"] = new { value };
+        }
+
+        public void SetCooldown(string category, double duration)
+        {
+            Components["minecraft:cooldown"] = new
+            {
+                category,
+                duration
+            };
+        }
+
+        public void Damage(int value)
+        {
+            Components["minecraft:damage"] = new { value };
+        }
+
+        public void Digger(bool useEfficiency, string tag, double speed)
+        {
+            Components["minecraft:digger"] = new
+            {
+                use_efficiency = useEfficiency,
+                destroy_speeds = new[]
+                {
+                    new { block = new { tags = tag }, speed }
+                }
+            };
+        }
+
+        public void DisplayName(string value)
+        {
+            Components["minecraft:display_name"] = new { value };
+        }
+
+        public void Durability(int minDamage, int maxDamage, int maxDurability)
+        {
+            Components["minecraft:durability"] = new
+            {
+                damage_chance = new { min = minDamage, max = maxDamage },
+                max_durability = maxDurability
+            };
+        }
+
+        public void Enchantable(string slot, int value)
+        {
+            Components["minecraft:enchantable"] = new { slot, value };
+        }
+
+        public void EntityPlacer(string entity, params string[] useOn)
+        {
+            Components["minecraft:entity_placer"] = new
+            {
+                entity,
+                use_on = useOn
+            };
+        }
+
+        public void Food(int nutrition, double saturationModifier, string usingConvertsTo)
+        {
+            Components["minecraft:food"] = new
+            {
+                nutrition,
+                saturation_modifier = saturationModifier,
+                using_converts_to = usingConvertsTo
+            };
+        }
+
+        public void Fuel(double duration)
+        {
+            Components["minecraft:fuel"] = new { duration };
+        }
+
+        public void Glint(bool value)
+        {
+            Components["minecraft:glint"] = new { value };
+        }
+
+        public void HandEquipped(bool value)
+        {
+            Components["minecraft:hand_equipped"] = new { value };
+        }
+
+        public void HoverTextColor(string color)
+        {
+            Components["minecraft:hover_text_color"] = color;
+        }
+
+        public void InteractButton(string text)
+        {
+            Components["minecraft:interact_button"] = text;
+        }
+
+        public void LiquidClipped(bool value)
+        {
+            Components["minecraft:liquid_clipped"] = new { value };
+        }
+
+        public void MaxStackSize(int value = 64)
+        {
+            Components["minecraft:max_stack_size"] = new { value };
+        }
+
+        public void Projectile(double minimumCriticalPower, string projectileEntity)
+        {
+            Components["minecraft:projectile"] = new
+            {
+                minimum_critical_power = minimumCriticalPower,
+                projectile_entity = projectileEntity
+            };
+        }
+
+        public void Record(int comparatorSignal, double duration, string soundEvent)
+        {
+            Components["minecraft:record"] = new
+            {
+                comparator_signal = comparatorSignal,
+                duration,
+                sound_event = soundEvent
+            };
+        }
+
+        public void Shooter(string item, bool useOffhand, bool searchInventory, bool useInCreative)
+        {
+            Components["minecraft:shooter"] = new
+            {
+                ammunition = new[]
+                {
+                    new
+                    {
+                        item,
+                        use_offhand = useOffhand,
+                        search_inventory = searchInventory,
+                        use_in_creative = useInCreative
+                    }
+                }
+            };
+        }
+
+        public void Wearable(string slot, bool dispensable)
+        {
+            Components["minecraft:wearable"] = new
+            {
+                slot,
+                dispensable
+            };
+        }
+    }
+}
